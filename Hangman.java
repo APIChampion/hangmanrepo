1403d9775eb1721af458bf1837e16e7e6d944e8d
Ben Rodriguez <benrodriguez9@gmail.com>
2017-11-08 19:38:36 -0600

    Add files via upload

--- Hangman.java
+++ Hangman.java
@@ -3,7 +3,12 @@ public class Hangman {
   public static void main(String[] args) {
     //my awesome code will eventually go here
     Game game = new Game("treehouse");
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit){
+      System.out.println("We got a hit");
+    } else {
+      System.out.println("oops missed");
+    }
   }
-
-
 }
